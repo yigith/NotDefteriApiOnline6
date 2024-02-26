3eb5f2fcfd4b2b5ddfae3a82248d2b5865dcced1
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2024-02-26 09:52:51 +0300

    yeni not

--- NotDefteriUi/site.js
+++ NotDefteriUi/site.js
@@ -4,25 +4,29 @@ const frmNot = document.getElementById("frmNot");
 const txtBaslik = document.getElementById("baslik");
 const txtIcerik = document.getElementById("icerik");
 const btnSil = document.getElementById("sil");
+const btnYeni = document.getElementById("yeni");
 let notlar = [];
 let seciliNot = null;
 
-function listele() {
-    divNotlar.innerHTML = "";
-
+function notlariGetir() {
     axios.get(apiUrl).then(res => {
         notlar = res.data;
-
-        for (const not of notlar) {
-            const a = document.createElement("a");
-            a.not = not;
-            a.href = "#";
-            a.className = "list-group-item list-group-item-action";
-            a.textContent = not.baslik;
-            a.onclick = (e) => notuGoster(not);
-            divNotlar.append(a);
-        }
+        notlariListele();
     });
+}
+
+function notlariListele() {
+    divNotlar.innerHTML = "";
+
+    for (const not of notlar) {
+        const a = document.createElement("a");
+        a.not = not;
+        a.href = "#";
+        a.className = "list-group-item list-group-item-action";
+        a.textContent = not.baslik;
+        a.onclick = (e) => notuGoster(not);
+        divNotlar.append(a);
+    }
 }
 
 function notuGoster(not) {
@@ -44,4 +48,13 @@ function secimiGuncelle() {
     });
 }
 
-listele();
+btnYeni.onclick = (e) => {
+    axios.post(apiUrl, { baslik: "Yeni Not", icerik: "" }).then(res => {
+        notlar.push(res.data);
+        seciliNot = res.data;
+        notlariListele();
+        notuGoster(res.data);
+    });
+};
+
+notlariGetir();
